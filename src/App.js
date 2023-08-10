b3700b96a906d61cf9a484f9928dc530f8b86919
Cassius Reynolds <cassius2828@gmail.com>
2023-08-10 13:25:05 -0700

    completed styling and responsiveness of part 1. Time to style part 2 and then add the route paths to navigate

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import calling from "./Images/calling.png";
+import tachyons from "tachyons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCheckCircle } from "@fortawesome/free-solid-svg-icons";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="main-container">
+        <div className="left-container">
+          <Left />
+        </div>
+        <div className="right-container">
+          <div>
+            <img src={calling} alt="video call" />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
 
 export default App;
+
+export const Left = () => {
+  return (
+    <div className="content-container">
+      <h1>Stay Updated!</h1>
+      <p>Join 60,000+ product managers receiving monthly updates on: </p>
+      <ul>
+        <li>
+          {" "}
+          <FontAwesomeIcon className="icon" icon={faCheckCircle} /> Product
+          discovery and bulding what matters
+        </li>
+        <li>
+          <FontAwesomeIcon className="icon" icon={faCheckCircle} />
+          Measuring to ensure updates are a success
+        </li>
+        <li>
+          <FontAwesomeIcon className="icon" icon={faCheckCircle} />
+          And much more
+        </li>
+      </ul>
+      <div className="email-container">
+        <label for="email">Email</label>
+        <input placeholder="enter email here" type="email" id="email" />
+        <div className="btn-container"><a href=""><button>Subscribe to a Monthly Newsletter</button></a></div>
+      </div>
+    </div>
+  );
+};
