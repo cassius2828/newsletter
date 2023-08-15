cbebbeee612a1174a2d3e5ae90d46956d4a31eb8
Cassius Reynolds <cassius2828@gmail.com>
2023-08-14 20:14:45 -0700

    completed all styling and paths for exercise

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import "./App.css";
 import calling from "./Images/calling.png";
 import tachyons from "tachyons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCheckCircle } from "@fortawesome/free-solid-svg-icons";
+import { faCheckCircle, faHome } from "@fortawesome/free-solid-svg-icons";
+import { Link } from "react-router-dom";
 
 function App() {
   return (
@@ -12,8 +13,8 @@ function App() {
           <Left />
         </div>
         <div className="right-container">
-          <div>
-            <img src={calling} alt="video call" />
+          <div className="img-container">
+            <img id="main-img" src={calling} alt="video call" />
           </div>
         </div>
       </div>
@@ -46,8 +47,49 @@ export const Left = () => {
       <div className="email-container">
         <label for="email">Email</label>
         <input placeholder="enter email here" type="email" id="email" />
-        <div className="btn-container"><a href=""><button>Subscribe to a Monthly Newsletter</button></a></div>
+        <div className="btn-container">
+          <Link to="/success">
+            {" "}
+            <a>
+              <button>Subscribe to a Monthly Newsletter</button>
+            </a>
+          </Link>
+        </div>
       </div>
     </div>
+  );
+};
+
+export const Success = () => {
+  return (
+    <>
+      <div className="App">
+        <div className="tc">
+          <Link to="/">
+            {" "}
+            <FontAwesomeIcon size="2x" className="icon home" icon={faHome} />
+          </Link>
+
+          <div className="main-container success">
+            <div className="content-container success">
+              <div className="icon-container">
+                <FontAwesomeIcon
+                  className="icon"
+                  size="3x"
+                  icon={faCheckCircle}
+                />
+              </div>
+              <h1>Thanks for Subscribing</h1>
+              <p>
+                a confirmation email has been sent to{" "}
+                <span>JohnDoe123@gmail.com</span>. Please open it and click the
+                button inside to confirm your subscription
+              </p>
+              <button>Lorem sample text</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
   );
 };
--- src/index.js
+++ src/index.js
@@ -1,13 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import App, {Success} from './App';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 
-
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "success",
+    element: <Success />,
+  },
+]);
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+   <RouterProvider router={router}/>
   </React.StrictMode>
 );
 
